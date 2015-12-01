3e3ab41d0702238081b8939f4e82cea8d2142649
Joerg Kubaile <jk@zettwerk.com>
2015-12-01 08:07:40 +0100

    finetuning

--- .emacs.d/general.el
+++ .emacs.d/general.el
@@ -36,3 +36,8 @@
       (lambda()
         (setq c-basic-offset 2)
         (setq indent-tabs-mode nil)))
+
+(setq browse-url-browser-function 'w3m-browse-url)
+(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+;; optional keyboard short-cut
+(global-set-key "\C-xm" 'browse-url-at-point)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,3 +17,22 @@
 (load-library "javascript")
 
 (auto-complete-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(background brightwhite)
+ '(column-number-mode t)
+ '(elpy-rpc-backend "jedi")
+ '(font-use-system-font t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(use-file-dialog nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
